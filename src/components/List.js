8f713a0927faf3b62d1428e70401de7ff849426b
Giuseppe Leone <gpleone@gmail.com>
2019-04-23 14:05:15 +0200

    code refactoring

--- src/components/List.js
+++ src/components/List.js
@@ -2,15 +2,21 @@ import React from 'react';
 
 import { Context } from '../store';
 
-const List = props => {
-  const { state } = React.useContext(Context);
+const List = () => {
+  const { actions, state } = React.useContext(Context);
 
   return (
-    <ul>
-      {state.projects.map(item => (
-        <li>{item.name}</li>
-      ))}
-    </ul>
+    <div>
+      {state.projects &&
+        state.projects.map(item => (
+          <p key={item.id}>
+            {item.name} {item.id}
+            <button onClick={() => actions.removeProject({ payload: item.id })}>
+              remove me
+            </button>
+          </p>
+        ))}
+    </div>
   );
 };
 
